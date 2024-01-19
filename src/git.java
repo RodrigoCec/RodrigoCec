844bf442fbd48024c3184e368af1ed05411e3134
Rodrigo Simon <rs2162715@gmail.com>
2024-01-18 20:08:19 -0600

    Se agrego la opcion de poder eleijir cuantos dados usar

--- src/git.java
+++ src/git.java
@@ -1,15 +1,23 @@
 import java.util.Random;
+import java.util.Scanner;
+
 public class git {
     public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Cuantos dados deseas usar: ");
+         int cantidadDados = scanner.nextInt();
  
         Random random = new Random();
-        Random random2 = new Random();
-        
-        int dado2 = random2.nextInt(6);
-        int numeroAleatorio = random.nextInt(6);
-        
-        
-        System.out.println("La cara del primer dado es: " + numeroAleatorio);
-        System.out.println("La cara del segundo dado es: " + dado2);
+
+        System.out.println("Resultados de los dados:");
+
+        for (int i = 0; i < cantidadDados; i++) {
+            int resultadoDado = random.nextInt(6) + 1;
+            System.out.println("Dado " + (i + 1) + ": " + resultadoDado);
+        }
+
+        scanner.close();
     }
 }
