8974c7cc182e7cbb99bd79bf658d17c74e9f6c71
Rodrigo Simon <rs2162715@gmail.com>
2024-01-18 20:03:18 -0600

    Se agrego la funcion de dos dados

--- src/git.java
+++ src/git.java
@@ -3,9 +3,13 @@ public class git {
     public static void main(String[] args) {
  
         Random random = new Random();
+        Random random2 = new Random();
         
+        int dado2 = random2.nextInt(6);
         int numeroAleatorio = random.nextInt(6);
-
-        System.out.println("La cara es: " + numeroAleatorio);
+        
+        
+        System.out.println("La cara del primer dado es: " + numeroAleatorio);
+        System.out.println("La cara del segundo dado es: " + dado2);
     }
 }
